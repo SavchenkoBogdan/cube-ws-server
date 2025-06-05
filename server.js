9c428f8168b2c8321e5457ba07c87db08c576c87
Savchenko_Bogdan <savchenkobogdan@gmail.com>
2025-06-06 00:12:59 +0300

    fix

--- server.js
+++ server.js
@@ -11,22 +11,31 @@ const wss = new WebSocket.Server({ server });
 let clients = new Set();
 
 wss.on('connection', (ws) => {
-  const id = Math.random().toString(36).substring(2);
-  ws.id = id;
   clients.add(ws);
-  console.log("Client connected:", id);
+  console.log("Client connected");
 
   ws.on('message', (msg) => {
     const text = msg.toString();
-    console.log(`[${id}]`, text);
 
-    // Broadcast to others
-    for (const client of clients) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(text);
-      }
+    let data;
+    try {
+        data = JSON.parse(text);
+    } catch (e) {
+        console.warn("Invalid JSON");
+        return;
+    }
+
+     if (data.event === "start_fall") {
+        for (const client of clients) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ event: "start_fall" }));
+            }
+        }
     }
   });
+
+
+
 
   ws.on('close', () => {
     console.log("Client disconnected:", id);
@@ -34,8 +43,11 @@ wss.on('connection', (ws) => {
   });
 });
 
-// Start the server on the port Render gives us (via process.env.PORT)
+
+
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+
